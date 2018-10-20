47be278fb5db93d5fbaff6d1cac0947f51a8c459
Tremaine Johns <Tremaine.A.Johns@gmail.com>
2018-10-20 09:00:49 -0400

    passed all tests

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(txt,number) {
+	
+	if(number < 0){
+		return 'ERROR';
+	}
 
+	let myString='';
+	for(let i=0;i<number;i++){
+		if(i===0){
+			myString = txt;
+		}
+		else{
+			myString += txt;
+		}
+	}
+	return myString;
 }
 
 module.exports = repeatString
