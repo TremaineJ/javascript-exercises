56d11b04cd99f63fb423bcaf8f91b4a25cb60886
Tremaine Johns <Tremaine.A.Johns@gmail.com>
2018-10-20 09:55:35 -0400

    Passed first test

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(myValues, remove) {
+	let initArray = [];
+	let newArray = initArray.concat(myValues);
 
+	newArray.splice(remove-1,1);
+
+	return newArray;
 }
 
 module.exports = removeFromArray
