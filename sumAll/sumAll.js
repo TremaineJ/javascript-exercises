8fdc2de80a9ee6dcc39af833b0d898a4208bd595
Tremaine Johns <Tremaine.A.Johns@gmail.com>
2018-10-21 11:03:22 -0400

    solution passes test 1

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(...args) {
 
+	function compareNumbers(a,b){
+		return a-b;
+	}
+
+	let array1 = Array.from(args);
+	let sortedArray = array1.sort(compareNumbers);
+	let lastValue = sortedArray.length - 1;
+	let sum = 0;
+
+	for(i=sortedArray[0]; i<=sortedArray[lastValue]; i++){
+		sum +=i;
+	}
+
+	return sum;
 }
 
 module.exports = sumAll
